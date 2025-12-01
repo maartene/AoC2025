2253b8bf3b0a48757c13e166e90fb8eec6aa0c11
Maarten Engels <maartene@mac.com>
2025-12-01 08:25:55 +0100

    R: actual calculation

--- Day01/Sources/Day01/Day01.swift
+++ Day01/Sources/Day01/Day01.swift
@@ -2,9 +2,47 @@
 // https://docs.swift.org/swift-book
 
 func getNumberOfRotationsEndingOnZero(for input: String) -> Int {
-    let endStates = [
-        50, 82, 52, 0, 95, 55, 0, 99, 0, 14, 32
+    let instructions = [
+        Instruction(direction: .left, amount: 68),
+        Instruction(direction: .left, amount: 30),
+        Instruction(direction: .right, amount: 48),
+        Instruction(direction: .left, amount: 5),
+        Instruction(direction: .right, amount: 60),
+        Instruction(direction: .left, amount: 55),
+        Instruction(direction: .left, amount: 1),
+        Instruction(direction: .left, amount: 99),
+        Instruction(direction: .right, amount: 14),
+        Instruction(direction: .left, amount: 82),
     ]
     
+    var endStates = [50]
+    
+    for instruction in instructions {
+        var currentPosition = endStates.last!
+        switch instruction.direction {
+        case .left:
+            currentPosition = currentPosition - instruction.amount
+            if currentPosition < 0 {
+                currentPosition += 100
+            }
+        case .right:
+            currentPosition = currentPosition + instruction.amount
+            if currentPosition > 99 {
+                currentPosition -= 100
+            }
+        }
+        endStates.append(currentPosition)
+    }
+    
     return endStates.count(where: { $0 == 0 })
+}
+
+struct Instruction {
+    enum Direction {
+        case left
+        case right
+    }
+    
+    let direction: Direction
+    let amount: Int
 }
