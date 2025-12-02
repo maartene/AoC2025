6f2d6f734da331a759ed5ac8017ada37b3599606
Maarten Engels <maartene@mac.com>
2025-12-02 07:09:07 +0100

    T: the invalid ID in the range should be the expected result

--- Day02/Tests/Day02Tests/Day02Tests.swift
+++ Day02/Tests/Day02Tests/Day02Tests.swift
@@ -4,16 +4,15 @@ import Testing
 let exampleInput =
     "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite struct `To get the first star on day 02` {
     @Test func `the sum of all invalid IDs in the example input should be 1227775554`() {
         #expect(sumOfInvalidIdsIn(exampleInput) == 1227775554)
     }
     
-    @Test func `the invalid ID in the range 95...105 should be 99`() {
-        #expect(invalidIDsIn(lowerBound: 95, upperBound: 105) == [99])
+    @Test(arguments: [
+        (95, 105, [99]),
+        (1698522, 1698528, [])
+    ]) func `the invalid ID in the range should be the expected result`(testcase: (lowerBound: Int, upperBound: Int, expectedResult: [Int])) {
+        #expect(invalidIDsIn(lowerBound: testcase.lowerBound, upperBound: testcase.upperBound) == testcase.expectedResult)
     }
 }
