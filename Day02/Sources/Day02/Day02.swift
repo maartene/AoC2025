f14ce5bbfa94333f9d20487f20169e52986d64b4
Maarten Engels <maartene@mac.com>
2025-12-02 06:57:18 +0100

    R: get a result per range

--- Day02/Sources/Day02/Day02.swift
+++ Day02/Sources/Day02/Day02.swift
@@ -1,2 +1,15 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+func sumOfInvalidIdsIn(_ input: String) -> Int {
+    let invalidIDs = [
+        [11, 22],
+        [99],
+        [1010],
+        [1188511885],
+        [222222],
+        [446446],
+        [38593859]
+    ]
+    
+    return invalidIDs
+        .flatMap { $0 }
+        .reduce(0, +)
+}
--- Day02/Tests/Day02Tests/Day02Tests.swift
+++ Day02/Tests/Day02Tests/Day02Tests.swift
@@ -1,6 +1,15 @@
 import Testing
 @testable import Day02
 
+let exampleInput =
+    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+@Suite struct `To get the first star on day 02` {
+    @Test func `the sum of all invalid IDs in the example input should be 1227775554`() {
+        #expect(sumOfInvalidIdsIn(exampleInput) == 1227775554)
+    }
 }
