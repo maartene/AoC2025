5a2c08f63a4f17c99d6b5b4ffa1617ea630ebd35
Maarten Engels <maartene@thedreamweb.eu>
2025-12-03 20:34:20 +0100

    Day04 scaffold setup

---
+++ Day04/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Day04",
+    platforms: [
+        .macOS(.v26)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Day04",
+            targets: ["Day04"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Shared", path: "../Shared")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Day04",
+            dependencies: ["Shared"]
+        ),
+        .testTarget(
+            name: "Day04Tests",
+            dependencies: ["Day04"]
+        ),
+    ]
+)
+
