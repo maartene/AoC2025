eaf72fd5605c1d2414e76f970eeb5a74840a064f
Maarten Engels <maartene@mac.com>
2025-12-03 09:07:18 +0100

    Day 03 second star ⭐️⭐️

--- Day03/Sources/Day03/Day03.swift
+++ Day03/Sources/Day03/Day03.swift
@@ -1,29 +1,62 @@
 import Shared
+import Foundation
 
-func maximumJoltageFrom(_ input: String) -> Int {
+func maximumJoltageFrom(_ input: String, maxDigits: Int = 2) -> Int {
     let banks = input.split(separator: "\n")
         .map { stringToIntArray($0) }
     
     let maximumPerBank = banks.map {
-        maximumJoltageFromBank($0)
+        cachedmaximumJoltageFromBank($0, maxDigits: maxDigits)
     }
     
     return maximumPerBank.reduce(0, +)
 }
 
-func maximumJoltageFromBank(_ bank: [Int]) -> Int {
-    var maximum = 0
-    
-    for leftIndex in 0 ..< bank.count - 1 {
-        for rightIndex in leftIndex + 1 ..< bank.count {
-            let batteryJoltage = bank[leftIndex] * 10 + bank[rightIndex]
-            
-            if batteryJoltage > maximum {
-                maximum = batteryJoltage
-            }
-        }
+struct JoltageInput: Hashable {
+    let bank: [Int]
+    let currentMaximum: Int
+    let startIndex: Int
+    let maxDigits: Int
+    let digitsCount: Int
+}
+
+func cachedmaximumJoltageFromBank(_ bank: [Int], maxDigits: Int) -> Int {
+    var cache: [String: Int] = [:]
+
+    return part2(bank: bank, digits: maxDigits, cache: &cache)
+}
+
+extension Array where Element == Int {
+    func toInt() -> Int {
+        reduce(0) { $0 * 10 + $1 }
     }
-    return maximum
+}
+
+func part2(bank: [Int], digits: Int, cache: inout [String: Int]) -> Int {
+    if let result = cache["\(bank)-\(digits)"] {
+        return result
+    }
+    
+    guard bank.count > 0 else {
+        return 0
+    }
+    
+    if bank.count == digits {
+        return bank.toInt()
+    }
+    
+//        # Take this digit
+//        # For the 12th digit, we need 10 ^ (12 - 1)
+    let a = bank[0] * pow(10, digits - 1) + part2(bank: Array(bank.dropFirst()), digits: digits - 1, cache: &cache)
+        
+    let b = part2(bank: Array(bank.dropFirst()), digits: digits, cache: &cache)
+    
+        
+    
+    // Return the greater value
+    let result = max(a, b)
+    cache["\(bank)-\(digits)"] = result
+    return result
 }
 
 func stringToIntArray(_ string: any StringProtocol) -> [Int] {
@@ -32,4 +65,16 @@ func stringToIntArray(_ string: any StringProtocol) -> [Int] {
             String($0)
         }
     return stringArray.compactMap { Int($0) }
+}
+
+func pow(_ base: Int, _ exponent: Int) -> Int {
+    guard exponent >= 0 else {
+        return 0
+    }
+    
+    var result = 1
+    for _ in 0 ..< exponent {
+        result *= base
+    }
+    return result
 }
--- Day03/Tests/Day03Tests/Day03Tests.swift
+++ Day03/Tests/Day03Tests/Day03Tests.swift
@@ -1,10 +1,6 @@
 import Testing
 @testable import Day03
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 let exampleInput =
 """
 987654321111111
@@ -24,11 +20,36 @@ let exampleInput =
         ("711111111111118", 78),
         ("234234234234278", 78),
         ("818181911112111", 92),
-    ]) func `the maximum joltage from bank 1 should be 98`(testcase: (bank: String, expected: Int)) {
-        #expect(maximumJoltageFromBank(stringToIntArray(testcase.bank)) == testcase.expected)
+    ]) func `the maximum joltage from banks for two digits`(testcase: (bank: String, expected: Int)) {
+        #expect(cachedmaximumJoltageFromBank( stringToIntArray(testcase.bank), maxDigits: 2) == testcase.expected)
     }
     
     @Test func `the maximum joltage from the actual input should be 17330`() {
         #expect(maximumJoltageFrom(input) == 17330)
+    }
+}
+
+@Suite struct `to get the second star on day 03` {
+    @Test(arguments: [
+        ("987654321111111", 987654321111),
+        ("811111111111119", 811111111119),
+        ("234234234234278", 434234234278),
+        ("818181911112111", 888911112111),
+    ]) func `the maximum joltage from banks from 12 digits`(testcase: (bank: String, expected: Int)) {
+        #expect(cachedmaximumJoltageFromBank(stringToIntArray(testcase.bank), maxDigits: 12) == testcase.expected)
+    }
+    
+    @Test func `the maximum joltage from the example input should be 3121910778619`() {
+        #expect(maximumJoltageFrom(exampleInput, maxDigits: 12) == 3121910778619)
+    }
+    
+    @Test(arguments: [
+        ("2343453422641331233623444434443422422234243434644344344333436434324443344243444547343426444313413747", 764443413747),
+    ]) func `the maximum joltage from a large bank`(testcase: (bank: String, expected: Int)) {
+        #expect(cachedmaximumJoltageFromBank( stringToIntArray(testcase.bank), maxDigits: 12) == testcase.expected)
+    }
+    
+    @Test func `the maximum joltage from the actual input should be 171518260283767`() {
+        #expect(maximumJoltageFrom(input, maxDigits: 12) == 171518260283767)
     }
 }
