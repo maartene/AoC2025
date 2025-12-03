47630b3c3b80a444f554ea9728dfc49b913f439f
Maarten Engels <maartene@mac.com>
2025-12-03 06:58:15 +0100

    R: calculate total using reduce

--- Day03/Sources/Day03/Day03.swift
+++ Day03/Sources/Day03/Day03.swift
@@ -1,6 +1,26 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Shared
 
 func maximumJoltageFrom(_ input: String) -> Int {
-    357
+    let bank1 = stringToIntArray("987654321111111")
+    
+    let maximumPerBank = [
+        maximumJoltageFromBank(bank1),
+        89,
+        78,
+        92
+    ]
+    
+    return maximumPerBank.reduce(0, +)
+}
+
+func maximumJoltageFromBank(_ bank: [Int]) -> Int {
+    98
+}
+
+func stringToIntArray(_ string: any StringProtocol) -> [Int] {
+    let stringArray = string.map { $0 }
+        .map {
+            String($0)
+        }
+    return stringArray.compactMap { Int($0) }
 }
