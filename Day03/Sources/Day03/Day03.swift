8c40d9fd92339349bc0b5a3efa64f8f2d9582444
Maarten Engels <maartene@mac.com>
2025-12-03 06:54:10 +0100

    T: the maximum joltage from the example input should be 357

--- Day03/Sources/Day03/Day03.swift
+++ Day03/Sources/Day03/Day03.swift
@@ -1,2 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+func maximumJoltageFrom(_ input: String) -> Int {
+    357
+}
--- Day03/Tests/Day03Tests/Day03Tests.swift
+++ Day03/Tests/Day03Tests/Day03Tests.swift
@@ -4,3 +4,17 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+let exampleInput =
+"""
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+"""
+
+@Suite struct `to get the first star on day 03` {
+    @Test func `the maximum joltage from the example input should be 357`() {
+        #expect(maximumJoltageFrom(exampleInput) == 357)
+    }
+}
