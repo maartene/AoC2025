558f15102590e96d2ef92d0a293bf064229f6515
Maarten Engels <maartene@mac.com>
2025-12-04 07:57:36 +0100

    Day 04 first star ⭐️

--- Day04/Sources/Day04/Day04.swift
+++ Day04/Sources/Day04/Day04.swift
@@ -1,6 +1,46 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Shared
 
 func numberOfAccessibleRolls(in input: String) -> Int {
-    13
+    var rolls = Set<Vector>()
+    let matrix = convertInputToMatrixOfCharacters(input)
+    for row in 0 ..< matrix.count {
+        for column in 0 ..< matrix[row].count {
+            if matrix[row][column] == "@" {
+                rolls.insert(Vector(x: column, y: row))
+            }
+        }
+    }
+    
+    var count = 0
+    for roll in rolls {
+        let neighbours = roll.neighbours8
+        let neighbourCount = neighbours.count(where: { rolls.contains($0) })
+        
+        if neighbourCount < 4 {
+            count += 1
+        }
+    }
+    
+    return count
+}
+
+extension Vector {
+    static var neighbours8: [Vector] {
+        [
+            Vector(x: 0, y: 1),
+            Vector(x: 1, y: 0),
+            Vector(x: -1, y: 0),
+            Vector(x: 0, y: -1),
+            Vector(x: 1, y: 1),
+            Vector(x: -1, y: -1),
+            Vector(x: -1, y: 1),
+            Vector(x: 1, y: -1),
+        ]
+    }
+    
+    var neighbours8: [Vector] {
+        Vector.neighbours8.map {
+            $0 + self
+        }
+    }
 }
--- Day04/Tests/Day04Tests/Day04Tests.swift
+++ Day04/Tests/Day04Tests/Day04Tests.swift
@@ -15,12 +15,12 @@ let exampleInput =
 @.@.@@@.@.
 """
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite struct `to get the first star on day 04` {
     @Test func `the number of rolls that can be accessed in the example input should be 13`() {
         #expect(numberOfAccessibleRolls(in: exampleInput) == 13)
+    }
+    
+    @Test func `the number of rolls that can be accessed in the actual input should be 1502`() {
+        #expect(numberOfAccessibleRolls(in: input) == 1502)
     }
 }
