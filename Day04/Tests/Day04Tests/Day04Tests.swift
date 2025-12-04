dfaff091b233f9c47fc0f8d00258c007f56807bf
Maarten Engels <maartene@mac.com>
2025-12-04 07:46:34 +0100

    T: the number of rolls that can be accessed in the example input should be 13

--- Day04/Tests/Day04Tests/Day04Tests.swift
+++ Day04/Tests/Day04Tests/Day04Tests.swift
@@ -1,6 +1,26 @@
 import Testing
 @testable import Day04
 
+let exampleInput =
+"""
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+@Suite struct `to get the first star on day 04` {
+    @Test func `the number of rolls that can be accessed in the example input should be 13`() {
+        #expect(numberOfAccessibleRolls(in: exampleInput) == 13)
+    }
 }
