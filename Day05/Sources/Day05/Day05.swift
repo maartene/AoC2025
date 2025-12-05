c28b2dc8f1e94a1236a29cf745b61209e765a033
Maarten Engels <maartene@thedreamweb.eu>
2025-12-05 08:06:56 +0100

    Day 05 First star ⭐️

--- Day05/Sources/Day05/Day05.swift
+++ Day05/Sources/Day05/Day05.swift
@@ -2,26 +2,36 @@
 // https://docs.swift.org/swift-book
 
 func numberOfFreshIngredients(in input: String) -> Int {
-    let ranges = [
-        3...5,
-        10...14,
-        16...20,
-        12...18,
-    ]
-    
-    let ingredients =
-    [
-        1,
-        5,
-        8,
-        11,
-        17,
-        32
-    ]
+    let parsedInput = getRangesAndIngredientsFrom(input)
+    let ingredients = parsedInput.ingredients
+    let ranges = parsedInput.ranges
     
     let freshIngredients = ingredients.filter { ingredient in
         ranges.contains(where: { $0.contains(ingredient) })
     }
     
     return freshIngredients.count
+}
+
+private func getRangesAndIngredientsFrom(_ input: String) -> (ranges: [ClosedRange<Int>], ingredients: [Int]) {
+    let lines = input.split(separator: "\n")
+    
+    var ranges = [ClosedRange<Int>]()
+    var ingredients = [Int]()
+    
+    for line in lines {
+        let splits = line.split(separator: "-")
+        switch splits.count {
+        case 2:
+            if let lowerBound = Int(splits[0]), let upperBound = Int(splits[1]) {
+                ranges.append(lowerBound...upperBound)
+            }
+        default:
+            if let ingredient = Int(splits[0]) {
+                ingredients.append(ingredient)
+            }
+        }
+    }
+    
+    return (ranges, ingredients)
 }
--- Day05/Tests/Day05Tests/Day05Tests.swift
+++ Day05/Tests/Day05Tests/Day05Tests.swift
@@ -20,4 +20,8 @@ let exampleInput =
     @Test func `the number of fresh ingredients in the example input should be 3`() {
         #expect(numberOfFreshIngredients(in: exampleInput) == 3)
     }
+    
+    @Test func `the number of fresh ingredients in the actual input should be 617`() {
+        #expect(numberOfFreshIngredients(in: input) == 617)
+    }
 }
