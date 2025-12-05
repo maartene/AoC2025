c260494f350dd6ae7411c09ed639a9cb944735c6
Maarten Engels <maartene@thedreamweb.eu>
2025-12-05 08:00:50 +0100

    T: the number of fresh ingredients in the example input should be 3

--- Day05/Tests/Day05Tests/Day05Tests.swift
+++ Day05/Tests/Day05Tests/Day05Tests.swift
@@ -1,6 +1,23 @@
 import Testing
 @testable import Day05
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+let exampleInput =
+"""
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+"""
+
+@Suite struct `to get the first star on day 05` {
+    @Test func `the number of fresh ingredients in the example input should be 3`() {
+        #expect(numberOfFreshIngredients(in: exampleInput) == 3)
+    }
 }
