4d4218ed9e097844419b9a81fe732614f484d19b
Maarten Engels <maartene@thedreamweb.eu>
2025-12-05 08:02:40 +0100

    R: calculate example input result

--- Day05/Sources/Day05/Day05.swift
+++ Day05/Sources/Day05/Day05.swift
@@ -2,5 +2,26 @@
 // https://docs.swift.org/swift-book
 
 func numberOfFreshIngredients(in input: String) -> Int {
-    3
+    let ranges = [
+        3...5,
+        10...14,
+        16...20,
+        12...18,
+    ]
+    
+    let ingredients =
+    [
+        1,
+        5,
+        8,
+        11,
+        17,
+        32
+    ]
+    
+    let freshIngredients = ingredients.filter { ingredient in
+        ranges.contains(where: { $0.contains(ingredient) })
+    }
+    
+    return freshIngredients.count
 }
