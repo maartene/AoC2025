b5a03634b9cd9946272c0ee6f1cfd363aa5705a7
Maarten Engels <maartene@thedreamweb.eu>
2025-12-06 07:19:44 +0100

    T: the total of all math problem answers summed up for the example input should be 4277556

--- Day06/Sources/Day06/Day06.swift
+++ Day06/Sources/Day06/Day06.swift
@@ -1,2 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+func sumOfMathProblems(in input: String) -> Int {
+    4277556
+}
--- Day06/Tests/Day06Tests/Day06Tests.swift
+++ Day06/Tests/Day06Tests/Day06Tests.swift
@@ -1,6 +1,20 @@
 import Testing
 @testable import Day06
 
+let exampleInput =
+"""
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+"""
+
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+@Suite struct `to get the first star on day 06` {
+    @Test func `the total of all math problem answers summed up for the example input should be 4277556`() {
+        #expect(sumOfMathProblems(in: exampleInput) == 4277556)
+    }
 }
