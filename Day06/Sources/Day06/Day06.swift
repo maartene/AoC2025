4a015509828327062e763f060131f63a81a78d75
Maarten Engels <maartene@thedreamweb.eu>
2025-12-06 07:33:41 +0100

    R: string parsing

--- Day06/Sources/Day06/Day06.swift
+++ Day06/Sources/Day06/Day06.swift
@@ -2,12 +2,18 @@
 // https://docs.swift.org/swift-book
 
 func sumOfMathProblems(in input: String) -> Int {
-    let answersToProblems = [
-        calculateProblem(["123", "45", "6", "*"]),
-        calculateProblem(["328", "64", "98", "+"]),
-        calculateProblem(["51", "387", "215", "*"]),
-        calculateProblem(["64", "23", "314", "+"])
-    ]
+    let problems = stringToColumns(input)
+    
+    let answersToProblems = problems.map {
+        calculateProblem($0)
+    }
+    
+//    let answersToProblems = [
+//        calculateProblem(["123", "45", "6", "*"]),
+//        calculateProblem(["328", "64", "98", "+"]),
+//        calculateProblem(["51", "387", "215", "*"]),
+//        calculateProblem(["64", "23", "314", "+"])
+//    ]
     
     return answersToProblems.reduce(0, +)
 }
@@ -30,5 +36,36 @@ func calculateProblem(_ input: [String]) -> Int {
         }
     }
     
+    return result
+}
+
+func stringToStringMatrix(_ input: String) -> [[String]] {
+    let lines = input.split(separator: "\n")
+    
+    var result = [[String]]()
+    
+    for line in lines {
+        let row = line.split(separator: " ")
+            .map { String($0) }
+        result.append(row)
+    }
+    
+    return result
+}
+
+func stringToColumns(_ input: String) -> [[String]] {
+    let stringMatrix = stringToStringMatrix(input)
+    
+    var result = [[String]]()
+    
+    for columnIndex in 0..<stringMatrix[0].count {
+        var column: [String] = []
+        for row in stringMatrix {
+            column.append(row[columnIndex])
+        }
+        result.append(column)
+    }
+    
+    print(result)
     return result
 }
