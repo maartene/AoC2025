8a47b3daeb20d42680c1b5e771104708374d884c
Maarten Engels <maartene@mac.com>
2025-12-07 12:10:37 +0100

    R: clarify variables

--- Day07/Sources/Day07/Day07.swift
+++ Day07/Sources/Day07/Day07.swift
@@ -4,30 +4,31 @@ func pathAndSplitCount(in input: String) -> (pathCount: Int, splitCount: Int) {
     let matrix = Matrix<Character>.makeCharacterMatrix(from: input)
     let startPosition = matrix[0].firstIndex { $0 == "S" }!
     
-    var beams: [Int: Int] = [startPosition: 1]
+    var beamPositionsAndStrength: [Int: Int] = [startPosition: 1]
     var splitCount = 0
     
     for y in 0 ..< matrix.height {
-        var updatedBeams = beams.reduce(into: [Int:Int]()) { partialResult, beam in
+        // zero out the updatedBeams
+        var updatedBeams = beamPositionsAndStrength.reduce(into: [Int:Int]()) { partialResult, beam in
             partialResult[beam.key] = 0
         }
         
-        for beam in beams {
-            let beamValue = beam.value
+        // check what happens for each beam
+        for beam in beamPositionsAndStrength {
             if matrix[y][beam.key] == "^" {
                 splitCount += 1
                 updatedBeams.removeValue(forKey: beam.key)
                 
-                updatedBeams[beam.key - 1, default: 0] += beamValue
-                updatedBeams[beam.key + 1, default: 0] += beamValue
+                updatedBeams[beam.key - 1, default: 0] += beam.value
+                updatedBeams[beam.key + 1, default: 0] += beam.value
             } else {
-                updatedBeams[beam.key, default: 0] += beamValue
+                updatedBeams[beam.key, default: 0] += beam.value
             }
         }
-        beams = updatedBeams
+        beamPositionsAndStrength = updatedBeams
     }
     
-    let pathCount = beams
+    let pathCount = beamPositionsAndStrength
         .values
         .reduce(0, +)
     
