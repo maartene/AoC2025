10969483a3ef11c21deefd58fbadd95bb2f40e3d
Maarten Engels <maartene@thedreamweb.eu>
2025-12-08 10:09:23 +0100

    Day 08 first star ⭐️

--- Day08/Sources/Day08/Day08.swift
+++ Day08/Sources/Day08/Day08.swift
@@ -1,16 +1,58 @@
 import Foundation
 import Shared
+import Algorithms
 
-func part1(_ input: String) -> Int {
-    let circuitSizes = [
+func part1(_ input: String, numberOfPairsToConnect: Int) -> Int {
+    let junctionBoxes = loadJunctionBoxes(input)
+    
+    var circuits = Set(junctionBoxes.map {
+        Circuit(junctionBoxes: [$0])
+    })
+    
+    let junctionBoxPairs = Set(junctionBoxes.combinations(ofCount: 2)
+        .map {
+            Pair(v1: $0[0], v2: $0[1])
+        }
+                               )
+    let pairsIncludingDistance = junctionBoxPairs.map { pair in
+        let distance = pair.v1.distanceTo(pair.v2)
+        return (pair, distance)
+    }
+    
+    let sortedPairs = pairsIncludingDistance.sorted(by: { $0.1 < $1.1 })
+        .map { $0.0 }
+    
+    // try and combine circuits
+    for i in 0 ..< numberOfPairsToConnect {
+        let pair = sortedPairs[i]
+        print(circuits.map { $0.junctionBoxes.count })
+        let circuit1 = circuits.first(where: { $0.junctionBoxes.contains(pair.v1) })
+        let circuit2 = circuits.first(where: { $0.junctionBoxes.contains(pair.v2) })
+        
+        if var circuit1, let circuit2, circuit1 != circuit2 {
+            // combine circuits
+            circuits.remove(circuit1)
+            circuits.remove(circuit2)
+            circuit1.junctionBoxes = circuit1.junctionBoxes.union(circuit2.junctionBoxes)
+            circuits.insert(circuit1)
+        }
+    }
+    
+    let circuitSizes = circuits.map { $0.junctionBoxes.count }
+     
+    let expectedCircuitSizes: Set = [
         5, 4, 2, 2, 1, 1, 1, 1, 1, 1, 1
-    ].shuffled()
+    ]
     
     let threeLargest = circuitSizes.sorted(by: >)
         .prefix(3)
         .reduce(1, *)
     
     return threeLargest
+}
+
+struct Circuit: Equatable, Hashable {
+    var junctionBoxes: Set<Vector3>
 }
 
 struct Vector3: Equatable, Hashable {
