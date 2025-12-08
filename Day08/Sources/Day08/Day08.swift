3d56141cec84165334e68e04d592eadf4bd37062
Maarten Engels <maartene@thedreamweb.eu>
2025-12-08 08:29:55 +0100

    R: calculate multiplication of three largest circuits

--- Day08/Sources/Day08/Day08.swift
+++ Day08/Sources/Day08/Day08.swift
@@ -1,6 +1,16 @@
+import Foundation
+
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
 func part1(_ input: String) -> Int {
-    40
+    let circuitSizes = [
+        5, 4, 2, 2, 1, 1, 1, 1, 1, 1, 1
+    ].shuffled()
+    
+    let threeLargest = circuitSizes.sorted(by: >)
+        .prefix(3)
+        .reduce(1, *)
+    
+    return threeLargest
 }
