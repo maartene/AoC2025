de14220af60623f7f85bf719403cb00c3128f590
Maarten Engels <maartene@mac.com>
2025-12-09 07:02:44 +0100

    R: calculate for example input

--- Day09/Sources/Day09/Day09.swift
+++ Day09/Sources/Day09/Day09.swift
@@ -1,3 +1,23 @@
+import Shared
+
 func largestRectangleArea(in input: String) -> Int {
-    50
+    let positions = parseInput(input)
+    
+    var maxArea = 0
+    for p1 in positions {
+        for p2 in positions {
+            let area = abs(1 + p1.x - p2.x) * abs(1 + p1.y - p2.y)
+            maxArea = max(maxArea, area)
+        }
+    }
+    
+    return maxArea
+}
+
+private func parseInput(_ input: String) -> [Vector] {
+    let matrix = convertStringToIntMatrix(input, separator: ",")
+    
+    return matrix.map { row in
+        Vector(x: row[0], y: row[1])
+    }
 }
--- Day09/Tests/Day09Tests/Day09Tests.swift
+++ Day09/Tests/Day09Tests/Day09Tests.swift
@@ -13,10 +13,6 @@ let exampleInput =
 7,3
 """
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
-
 @Suite struct `to get the first star on day 09` {
     @Test func `the largest rectangle area for the example input should be 50`() {
         #expect(largestRectangleArea(in: exampleInput) == 50)
