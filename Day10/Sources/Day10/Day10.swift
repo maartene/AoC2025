5c1042c3fbdda7506d33780319ae7f70e5637dab
Maarten Engels <maartene@mac.com>
2025-12-10 07:19:21 +0100

    T: the fewest number of example presses in the example input should be 7

--- Day10/Sources/Day10/Day10.swift
+++ Day10/Sources/Day10/Day10.swift
@@ -1,9 +1,39 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func fewestNumberOfPresses(in input: String) -> Int {
+    7
+}
+
 struct Machine {
     let lights: [Bool]
     let rules: [Set<Int>]
+    
+    init(_ machineString: String) {
+        let parts = machineString.split(separator: " ")
+        var rules = [Set<Int>]()
+        
+        var lights = [Bool]()
+        
+        for part in parts {
+            switch part.first! {
+            case "[":
+                lights = part.dropFirst().dropLast()
+                    .map { $0 }
+                    .map { $0 == "#" ? true : false }
+            case "(":
+                let rule = part.dropFirst().dropLast()
+                    .split(separator: ",")
+                    .compactMap { Int($0) }
+                rules.append(Set(rule))
+            default:
+                break
+            }
+        }
+        
+        self.lights = lights
+        self.rules = rules
+    }
     
     init(lights: [Bool], rules: [Set<Int>]) {
         self.lights = lights
@@ -29,3 +59,5 @@ struct Machine {
         return minimumPresses
     }
 }
+
+extension Machine: Equatable { }
--- Day10/Tests/Day10Tests/Day10Tests.swift
+++ Day10/Tests/Day10Tests/Day10Tests.swift
@@ -1,9 +1,12 @@
 import Testing
 @testable import Day10
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
+let exampleInput =
+"""
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+"""
 
 @Suite struct `to get the first star on day 10` {
     @Test func `the fewest number of button presses for an example machine should be 2`() {
@@ -17,5 +20,22 @@ import Testing
         let result = machine.minimumButtonPresses()
         
         #expect(result == 2)
+    }
+    
+    @Test func `construct a machine from a string`() {
+        let expectedMachine = Machine(
+            lights: [false, true, true, false],
+            rules: [
+                [3], [1,3],[2],[2,3],[0,2],[0,1]
+            ]
+        )
+        
+        let machine = Machine("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")
+        
+        #expect(machine == expectedMachine)
+    }
+    
+    @Test func `the fewest number of example presses in the example input should be 7`() {
+        #expect(fewestNumberOfPresses(in: exampleInput) == 7)
     }
 }
