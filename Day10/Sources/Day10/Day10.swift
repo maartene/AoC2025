1756ecedc95e6a83f2c58d9c3bb94e5da09d57eb
Maarten Engels <maartene@mac.com>
2025-12-10 06:59:17 +0100

    T: the fewest number of button presses for an example machine should be 2

--- Day10/Sources/Day10/Day10.swift
+++ Day10/Sources/Day10/Day10.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+struct Machine {
+    var lights: [Bool]
+    var rules: [Set<Int>]
+    
+    func minimumButtonPresses() -> Int {
+        2
+    }
+}
--- Day10/Tests/Day10Tests/Day10Tests.swift
+++ Day10/Tests/Day10Tests/Day10Tests.swift
@@ -4,3 +4,16 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+@Suite struct `to get the first star on day 10` {
+    @Test func `the fewest number of button presses for an example machine should be 2`() {
+        let machine = Machine(
+            lights: [false, true, true, false],
+            rules: [
+                [3], [1,3],[2],[2,3],[0,2],[0,1],[3,5,4,7]
+            ]
+        )
+        
+        #expect(machine.minimumButtonPresses() == 2)
+    }
+}
