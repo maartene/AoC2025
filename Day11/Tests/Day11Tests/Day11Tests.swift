56c83a1d34e848901125e2560bda8ab72ee81cad
Maarten Engels <maartene@mac.com>
2025-12-11 06:54:17 +0100

    T: the number of paths from you to out for the example input should be 5

--- Day11/Tests/Day11Tests/Day11Tests.swift
+++ Day11/Tests/Day11Tests/Day11Tests.swift
@@ -1,6 +1,26 @@
 import Testing
 @testable import Day11
 
+let exampleInput =
+"""
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+"""
+
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
+@Suite struct `to get the first star on day 11` {
+    @Test func `the number of paths from you to out for the example input should be 5`() {
+        #expect(part1(exampleInput) == 5)
+    }
 }
