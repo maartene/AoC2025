b933a31939cfd6aea4d3c7ab0e719231f720f2e9
Maarten Engels <maartene@mac.com>
2025-12-11 08:47:11 +0100

    Day 09 second star ⭐️⭐️

--- Day09/Sources/Day09/Day09.swift
+++ Day09/Sources/Day09/Day09.swift
@@ -3,7 +3,7 @@ import Shared
 @main
 struct Day09 {
     static func main() {
-        let area = part2(in: input, startPoint: Vector(x: 50302, y: 1906), endY: 49476)
+        let area = part2(in: input)
         print(area)
     }
 }
@@ -30,111 +30,74 @@ func parseInput(_ input: String) -> [Vector] {
     }
 }
 
-func part2(in input: String, startPoint: Vector, endY: Int) -> Int {
-    let redCarpetPositions = parseInput(input)
-    let greenCarpetPositions = getGreenCarpetPositions(from: redCarpetPositions, startPoint: startPoint)
+struct Square {
+    let minX: Int
+    let maxX: Int
+    let minY: Int
+    let maxY: Int
     
-    var maxArea = 0
-    print("Startpoint: \(startPoint), endY: \(endY)")
-    
-    for y in startPoint.y ..< endY {
-        var x = startPoint.x
-        while greenCarpetPositions.contains(Vector(x: x, y: y)) == false {
-            x -= 1
-        }
-        let minX = x
-        
-        // grow right
-        x = startPoint.x
-        while greenCarpetPositions.contains(Vector(x: x, y: y)) == false {
-            x += 1
-        }
-        let maxX = x
-        
-        let area = abs(maxX + 2 - minX) * abs(1 + endY - y)
-        if area > maxArea {
-            print("Found new max area: \(area) \(minX)...\(maxX)  \(y)...\(endY)")
-            maxArea = max(area, maxArea)
-        }
-        
-    }
-    
-    return maxArea
-}
-
-func testInnerRectangleArea(p1: Vector, p2: Vector, redCarpetPositions: [Vector], greenCarpetPositions: Set<Vector>) -> Bool {
-    
-    var rectangeCoordinates = Set<Vector>()
-    for row in min(p1.y, p2.y) ... max(p1.y, p2.y) {
-        for col in min(p1.x, p2.x) ... max(p1.x, p2.x) {
-            rectangeCoordinates.insert(Vector(x: col, y: row))
-        }
-    }
-    
-    // all the other ones should be fully red or fully green. i.e. don't overlap
-    return rectangeCoordinates.allSatisfy { coordinate in
-        redCarpetPositions.contains(coordinate) || greenCarpetPositions.contains(coordinate)
-    }
-}
-
-func getGreenCarpetPositions(from redCarpetPositions: [Vector], startPoint: Vector) -> Set<Vector> {
-    var result = Set<Vector>()
-    for i in 0 ..< redCarpetPositions.count {
-        let p1 = redCarpetPositions[i]
-        let p2 = redCarpetPositions[(i + 1) % redCarpetPositions.count]
-        
-        if p1.x == p2.x {
-            // connect horizontally
-            for y in min(p1.y, p2.y) ... max(p1.y, p2.y) {
-                result.insert(Vector(x: p1.x, y: y))
+    func isLineCrossing(_ line: Square) -> Bool {
+        if line.maxX - line.minX == 0 {
+            if minX < line.minX && line.maxX < maxX {
+                // check if the x coordinate is between the bounds of the square
+                return minY < line.maxY && maxY > line.minY
+                // does the line cross the square on the y?
             }
         } else {
-            // connect vertically
-            for x in min(p1.x, p2.x) ... max(p1.x, p2.x) {
-                result.insert(Vector(x: x, y: p1.y))
+            if minY < line.minY && line.maxY < maxY {
+                // check if the y coordinate is between the bounds of the square
+                return minX < line.maxX && maxX > line.minX
+                // does the line cross the square on the x?
             }
         }
+        return false
     }
-//    
-//    // find point within polygon
-//    let minX = redCarpetPositions.map { $0.x }.min()!
-//    let minY = redCarpetPositions.map { $0.y }.min()!
-//    let maxX = redCarpetPositions.map { $0.x }.max()!
-//    let maxY = redCarpetPositions.map { $0.y }.max()!
-//    
-//    // flood fill rest of polygon
-//    var fill: Set = [startPoint]
-//    while let point = fill.popFirst() {
-//        result.insert(point)
-//        let neighbours = point.neighbours
-//            .filter {
-//                $0.x >= minX && $0.x <= maxX && $0.y >= minY && $0.y <= maxY
-//            }
-//            .filter { result.contains($0) == false }
-//        
-//        fill = fill.union(neighbours)
-//    }
-    printCarpets(carpetPositions: result)
+    
+    var area: Int {
+        (maxX - minX + 1) * (maxY - minY + 1)
+    }
+}
+
+extension Square: CustomStringConvertible {
+    var description: String {
+        "(\(minX),\(minY)):(\(maxX),\(maxY))"
+    }
+}
+
+public func part2(in input: String) -> Int {
+    let points = parseInput(input)
+    
+    var allLines = [Square]()
+    
+    for i in 0 ..< points.count {
+        let minX = min(points[i].x, points[(i+1)%points.count].x)
+        let maxX = max(points[i].x, points[(i+1)%points.count].x)
+        let minY = min(points[i].y, points[(i+1)%points.count].y)
+        let maxY = max(points[i].y, points[(i+1)%points.count].y)
         
+        allLines.append(Square(minX: minX, maxX: maxX, minY: minY, maxY: maxY))
+    }
+    
+    var result = 0
+    
+    for i in 0 ..< points.count {
+        next: for j in i + 1 ..< points.count {
+            let minX = min(points[i].x, points[j].x)
+            let maxX = max(points[i].x, points[j].x)
+            let minY = min(points[i].y, points[j].y)
+            let maxY = max(points[i].y, points[j].y)
+            
+            let square = Square(minX: minX, maxX: maxX, minY: minY, maxY: maxY)
+            
+            for line in allLines {
+                if square.isLineCrossing(line) {
+                    continue next
+                }
+            }
+            
+            result = max(result, square.area)
+        }
+    }
+    
     return result
-}
-
-func isWithinPolygon(_ testPoint: Vector, polygon: [Vector], maxX: Int) -> Bool {
-    var count = 0
-    for _ in testPoint.x ... maxX {
-        if polygon.contains(testPoint) {
-            count += 1
-        }
-    }
-    return count.isMultiple(of: 2) == false
-}
-
-func printCarpets(carpetPositions: Set<Vector>) {
-    for y in 0 ..< 16 {
-        var line = ""
-        for x in 0 ..< 24 {
-            line += carpetPositions.contains(Vector(x: x, y: y)) ? "#" : "."
-        }
-        print(line)
-    }
 }
--- Day09/Tests/Day09Tests/Day09Tests.swift
+++ Day09/Tests/Day09Tests/Day09Tests.swift
@@ -29,38 +29,7 @@ let exampleInput =
         #expect(part2(in: exampleInput) == 24)
     }
 
-    // 1432297642
-    // 1383178986
-    // 1383178986
-    // 2939369478
-    // 1383390016 not correct
-    // 1173523000 too low
-    // 668574442 too low
-    // 2323640150 too high
-    @Test func `the largest rectangle area for the actual input should be 24`() {
-        #expect(part2(in: input) == 24)
+    @Test func `the largest rectangle area for the actual input should be 1429596008`() {
+        #expect(part2(in: input) == 1429596008)
     }
-    
-    @Test func `elipse data`() {
-        let positions = parseInput(input)
-        let minX = positions.map { $0.x }.min()!
-        let minY = positions.map { $0.y }.min()!
-        let maxX = positions.map { $0.x }.max()!
-        let maxY = positions.map { $0.y }.max()!
-        
-        print(minX, minY)
-        
-        let horizontalRadius = (maxX - minX) / 2
-        let verticalRadius = (maxY - minY) / 2
-        
-        print("horizontalRadius: \(horizontalRadius) \nverticalRadius: \(verticalRadius)")
-        
-    }
-    
-//    @Test func `greentile count`() {
-//        let redCarpetTiles = parseInput(input)
-//        let greenCarpetTiles = getGreenCarpetPositions(from: redCarpetTiles, startPodxint: Vector(x: 10000, y: 60000))
-//        print(greenCarpetTiles.count)
-//        
-//    }
 }
